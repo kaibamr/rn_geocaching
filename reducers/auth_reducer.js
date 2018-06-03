0039f6b4d3997372bd16c6430634d728001c8607
kaibamr <wmrozek@newforma.com>
2018-06-03 15:33:33 +0200

    Added dumb auth reducer

---
+++ reducers/auth_reducer.js
@@ -0,0 +1,14 @@
+import {
+	FACEBOOK_LOGIN_SUCCESS
+} from '../actions/types';
+
+export default function(state = {}, action) {
+	switch (action.type) {
+		case FACEBOOK_LOGIN_SUCCESS:
+			return {
+				token: action.payload
+			};
+		default:
+			return state;
+	}
+}
