6da40c0c76b1a42e707e96d5add4add33b2ea875
kaibamr <wmrozek@newforma.com>
2018-06-12 20:22:44 +0200

    Added styles

--- screens/ResumeScreen.js
+++ screens/ResumeScreen.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, CardSection } from 'react-native';
+import _ from 'lodash';
 import { connect } from 'react-redux';
 import { Constants, MapView, Location, Permissions } from 'expo';
 import { setPosition } from '../actions/geo_actions';
+import { riddlesFetch,  setCurrentStep, setCompleted } from '../actions/riddles_actions';
+import riddles from "../reducers/riddles_reducer";
 
 class ResumeScreen extends Component {
 	state = {
@@ -19,25 +22,34 @@ class ResumeScreen extends Component {
 
 	componentDidMount() {
 		this._getLocationAsync();
+		this.props.riddlesFetch();
 	}
 
+
 	_handleMapRegionChange = mapRegion => {
-		this.setState({ mapRegion });
+		this.setState({mapRegion});
 	};
 
 	_getLocationAsync = async () => {
-		let { status } = await Permissions.askAsync(Permissions.LOCATION);
+		let {status} = await Permissions.askAsync(Permissions.LOCATION);
 		if (status !== 'granted') {
 			this.setState({
 				locationResult: 'Permission to access location was denied',
 			});
 		} else {
-			this.setState({ hasLocationPermissions: true });
+			this.setState({hasLocationPermissions: true});
 		}
 
-		Location.watchPositionAsync({ enableHighAccuracy: true, distanceInterval: 1}, (location) => {
-			this.setState({ locationResult: JSON.stringify(location)});
-			this.setState({mapRegion: { latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }});
+		Location.watchPositionAsync({enableHighAccuracy: true, distanceInterval: 1}, (location) => {
+			this.setState({locationResult: JSON.stringify(location)});
+			this.setState({
+				mapRegion: {
+					latitude: location.coords.latitude,
+					longitude: location.coords.longitude,
+					latitudeDelta: 0.0922,
+					longitudeDelta: 0.0421
+				}
+			});
 			this.props.setPosition({
 				latitude: location.coords.latitude,
 				longitude: location.coords.longitude
@@ -46,68 +58,78 @@ class ResumeScreen extends Component {
 			console.log('longitude ', this.props.longitude);
 			console.log('Dystans: ', (this.distance(this.props.latitude, this.props.longitude, 50.289557, 18.680221) * 1000));
 		});
+		console.log(this.props.riddlesMapped);
 	};
 
 	distance(lat1, lon1, lat2, lon2) {
 		const p = 0.017453292519943295;    // Math.PI / 180
 		const c = Math.cos;
-		const a = 0.5 - c((lat2 - lat1) * p)/2 +
+		const a = 0.5 - c((lat2 - lat1) * p) / 2 +
 			c(lat1 * p) * c(lat2 * p) *
-			(1 - c((lon2 - lon1) * p))/2;
+			(1 - c((lon2 - lon1) * p)) / 2;
 
 		return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
 	}
 
+	renderRiddleInfo() {
+		return this.props.riddlesMapped.length !== 0 ? this.props.riddlesMapped.map((riddle) => {
+			if (this.props.currentRiddle == riddle.id) {
+				return (
+					<Text>
+						Current Riddle: {riddle.description}
+					</Text>
+				);
+			}
+		}) : null;
+	}
+
 	render() {
 		return (
-			<View style={styles.container}>
-				<Text style={styles.paragraph}>
-					info
-				</Text>
-
-				{
-					this.state.locationResult === null ?
-						<Text>Finding your current location...</Text> :
-						this.state.hasLocationPermissions === false ?
-							<Text>Location permissions are not granted.</Text> :
-							this.state.mapRegion === null ?
-								<Text>Map region doesn't exist.</Text> :
-								<MapView
-									style={{ alignSelf: 'stretch', height: 400 }}
-									region={this.state.mapRegion}
-									onRegionChange={this._handleMapRegionChange}
-									showsMyLocationButton={true}
-									showsUserLocation={true}
-								/>
-				}
-			</View>
-
+				<View style={styles.container}>
+					{
+						this.state.locationResult === null ?
+							<Text>Finding your current location...</Text> :
+							this.state.hasLocationPermissions === false ?
+								<Text>Location permissions are not granted.</Text> :
+								this.state.mapRegion === null ?
+									<Text>Map region doesn't exist.</Text> :
+									<MapView
+										style={{alignSelf: 'stretch', height: 400}}
+										region={this.state.mapRegion}
+										onRegionChange={this._handleMapRegionChange}
+										showsMyLocationButton={true}
+										showsUserLocation={true}
+									/>
+					}
+				</View>
 		);
 	}
 }
 
-const styles = StyleSheet.create({
-	container: {
+
+const styles = {
+	map: {
 		flex: 1,
 		alignItems: 'center',
 		justifyContent: 'center',
-		paddingTop: Constants.statusBarHeight,
 		backgroundColor: '#ecf0f1',
 	},
-	paragraph: {
-		margin: 24,
-		fontSize: 18,
-		fontWeight: 'bold',
-		textAlign: 'center',
-		color: '#34495e',
-	},
-});
+	riddleInfo: {
+	}
+};
 
-function mapStateToProps({ geo }) {
+function mapStateToProps({ geo,  riddles}) {
+	const riddlesMapped = _.map(riddles.riddles, (val) => {
+		return { ...val };
+	});
+
 	return {
 		latitude: geo.latitude,
-		longitude: geo.longitude
+		longitude: geo.longitude,
+		currentRiddle: riddles.currentRiddle,
+		currentStep: riddles.currentStep,
+		riddlesMapped
 	};
 }
 
-export default connect(mapStateToProps, { setPosition })(ResumeScreen);
+export default connect(mapStateToProps, { setPosition, riddlesFetch,  setCurrentStep, setCompleted })(ResumeScreen);
